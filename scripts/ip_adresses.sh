adf4a4620c32bcb6630f77b3c346925289c6e7fe
AtanasKisyov <atanaskisyov9@gmail.com>
2022-06-28 21:21:40 +0300

    minor updates

--- scripts/ip_adresses.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bashresult.txt
-
-connections="$(grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' result.txt | sort | uniq )"
-
-for ip in $connections
-do
-  count="$(grep -oE "$ip" result.txt | wc -l)"
-echo "$ip" - "$count"
-done
--- scripts/processes.sh
+++ scripts/processes.sh
@@ -5,7 +5,7 @@ disk_space=$(df)
 last_login=$(last -5)
 processes=$(ps | grep -oE 'Python\|Perl'| wc -l)
 
-function get_cpu_cores () {
+get_cpu_cores () {
   echo "Count of CPU Cores: $number_of_cpu_cores"
 }
 
@@ -22,7 +22,7 @@ get_processes () {
   echo "Running Python/Perl Processes: $processes"
 }
 
-function caller () {
+caller () {
   echo "
 --------------------------------------------------------------------------------------
 
